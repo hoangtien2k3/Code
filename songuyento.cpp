5fd9dde2c683108f905c275a03aca10949cea65f
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2021-12-25 18:08:04 +0700

    first commit

--- songuyento.cpp
+++ songuyento.cpp
@@ -8,58 +8,18 @@ các số nguyên tố > số 2 đều là các số nguyên tố lẻ
 //                                 => nếu có lớn hơn 2 ước thì không phải là số nguyên tố
 
 
-// #include<stdio.h>
-// #include<math.h>
-
-// int songuyento (int n) {
-//     if (n < 2) {
-//         printf ("%d khong phai la so nguyen to", n);
-//         return 0;
-//     }
-//     int count = 0;
-//     for (int i = 2; i < n; i++) {
-//         if (n % i == 0) {
-//             count++;
-//             printf ("\n%d chia het cho %d", n, i);
-//         }
-//     }
-
-//     if (count == 0) {
-//         printf ("\n%d la so nguyen to", n);
-//     } else {
-//         printf ("\n%d khong phai la so nguyen to ", n);
-//     }
-
-//     return 0;
-// }
-
-// int main() {
-//     int n;
-//     int count = 0;
-//     printf ("nhap vao n = ");
-//     scanf ("%d", &n);
-
-//     // songuyento(n);
-//     printf ("%d", songuyento(n));
-
-//     return 0;
-// }
-
-
 
 // CÁCH ĐƠN GIẢN NHẤT ĐỂ LÀM BÀI NÀY ĐÓ LÀ :
 #include <stdio.h>
+#include<math.h>
 
-int main() {
-    int n, count = 0;
-    printf ("nhap vao n = ");
-    scanf ("%d", &n);
-
+int songuyento (int n) {
+    int count = 0;
     if (n < 2) {
-        printf ("\n%d khong phai la so nguyen to ", n);
+        printf ("\n%d KHONG PHAI LA SO NGUYEN TO ", n);
     }
 
-    for (int i = 2; i < n; i++) { 
+    for (int i = 2; i < n; i++) {  // i < n-1 // i <= sqrt(n) ;
         /* có thể viết i < sqrt(n) vì mỗi số đều có 2 miền giá trị nên ta chỉ cần xét 1 miền gí trị
         mà không nhất thiết cần phải xét tất cả các gí trị đến n;
         sqrt(n) lấy căn bậc 2 của "n" thì ta có thể đỡ phải chạy nhiều vòng for, có thể tối ưu được code khi chạy;                              
@@ -70,10 +30,27 @@ int main() {
         }
     }
     if (count == 0) {
-        printf ("\n%d la so nguyen to ", n);
+        printf ("\n%d LA SO NGUYEN TO ", n);
     } else {
-        printf ("\n%d khong phai la so nguyen to ", n);
+        printf ("\n%d KHONG PHAI LA SO NGUYEN TO ", n);
     }
+
+    return 0;
+}
+
+int main()
+{
+    int n, count = 0;
+    do
+    {
+        printf("Nhap vao n (n > 0) = ");
+        scanf("%d", &n);
+        // if (n < 0) {
+        //     printf ("Xin nhap lai, n (n > 0) = ");
+        // }
+    } while (n < 0 && printf("\nXin nhap lai!: "));
+
+    songuyento(n);
 
     return 0;
 }
---
+++ timgiatridu.cpp
@@ -0,0 +1,20 @@
+// TÌM GIÁ TRỊ DƯ CỦA PHÉP CHIA 
+//  giá trị dư khi chia dư 5 / 2 dư 1 
+//  % là phép chia lấy dư : 5 % 2 = 1
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout << "nhap vao gia tri a va b :" << endl;
+    cout << "nhap vao gia tri a = ";
+    cin >> a;
+    cout << "\nnhap vao gia tri b = ";
+    cin >> b;
+
+    cout << "So du cua :" << "a % b = " << a % b << endl;
+
+    return 0;
+}
