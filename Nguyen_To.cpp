7dd353e2f1524cfc35a36248b98dba5719567cf3
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-19 01:57:58 +0700

    Code_

---
+++ Nguyen_To.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+#include<math.h>
+
+bool nt(int a) {
+    for (int i=2; i<=sqrt(a); i++) {
+        if (a % i == 0) {
+            return false;
+        }
+    }
+    return a>1;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    int a[n];
+    for (int i=0; i<n; i++) {
+        std::cin >> a[i];
+    }
+
+    for (int i=0; i<n; i++)  {
+        if (nt(a[i])) {
+            std::cout << a[i] <<" ";
+        }
+    }
+    return 0;
+}
