c0977a6d9160301b666ca65d0e09ba1544f6d445
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-22 21:00:58 +0700

    Sum Element Array _ 2

---
+++ TongPhanTuCuaMang1Chieu.cpp
@@ -0,0 +1,39 @@
+
+#include<iostream>
+
+Cách 1: dùng đệ quy
+int SumArray1(int *a, int n) {
+    if (n == 1)
+        return *a;
+    return a[n-1] + SumArray1(a, n-1);
+}
+
+// Cách 2: dùng vòng lặp
+// int SumArray1(int *a, int n) {
+//     int sum = 0;
+//     for (int i=0; i<n; i++) {
+//         sum += a[i];
+//     }
+//     return sum;
+// }
+
+// Cách 3: 
+// int SumArray1(int *a, int left, int right) {
+//     if (left == right) 
+//         return a[left];
+//     int mid = (left + right)/2;
+//     return SumArray1(a, left, mid) + SumArray1(a, mid+1, right);
+// }
+
+int main() {
+    int n;
+    int left, right;
+    printf ("Input n = ");
+    scanf ("%d", &n);
+    int arr[n];
+    for (int i=0; i<n; i++) {
+        scanf ("%d", &arr[i]);
+    }
+    printf ("Sum Element Array: %d", SumArray1(arr, n));
+    return  0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,5 @@
-
-    cout << "Nhap cac phan tu mang: ";
+int SumArray1(int *a, int n) {
+//     if (n == 1)
+//         return *a;
+//     return a[n-1] + SumArray1(a, n-1);
+// }
