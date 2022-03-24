203e9e2efabe174230ff088456f0a1015909bd74
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-24 09:43:35 +0700

    Code thieu nhi

--- 1000 Bài Code Thiếu Nhi/Chương 3/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-    // for (int i=1; i<=10000; i++) {
-    //     sum += i;
-    //     if (sum >= 10000) {
-    //         printf ("n can tim la %d", i);
-    //         break;
-    //     }
-    // }
