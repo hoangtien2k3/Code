5b25db12935a1340dd364e350a2dab7ea94bb030
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-05-28 11:35:58 +0700

    C 28/05/2022

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+    // // xóa khoản trắng ở vị trí thân chuỗi
+    // for(int i=0; i<strlen(str) - 1; i++) {
+    //     if (str[i] == ' ' && str[i + 1] == ' ') {
+    //         strcpy(&str[i], &str[i + 1]);
+    //         i--;
+    //     } 
+    // }
