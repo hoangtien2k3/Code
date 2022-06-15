c56293ae8bc186a95e14d48cdea3799716fe67c3
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-06-15 14:33:55 +0700

    C++ 15/06/2022

--- SoLanXuatHienCuaPhanTuMang_C2.cpp
+++ SoLanXuatHienCuaPhanTuMang_C2.cpp
@@ -1,34 +1,68 @@
-// đến số lần xuất hiện của các phần tử trong mảng
+// // đến số lần xuất hiện của các phần tử trong mảng
 
+// #include <iostream>
+// using namespace std;
+ 
+// const int MAX = 1e6; // 1e6 = 1 * 10^6
+// int cnt[MAX];
+
+// int main(){
+//     int n;
+//     do{
+//         cout << "\nNhap n = ";
+//         cin >> n;
+//     }while(n < 1);
+//     int a[n];
+//     for(int i = 0; i < n; i++){
+//         do{
+//             cout << "arr[" << i << "] = ";
+//             cin >> a[i];
+//         }while(a[i] < 0);
+//     }
+//     for(int i = 0; i < MAX; i++) 
+//         cnt[i] = 0;
+//     for(int i = 0; i < n; i++){
+//         cnt[a[i]]++;
+//     }
+//     for(int i = 0; i < MAX; i++){
+//         if(cnt[i] > 0){
+//             cout << "Gia tri " << i << " xuat hien " << cnt[i] << " lan!\n";
+//         }
+//     }
+// }
+
+
+
+// Tìm tần xuất lẻ xuất hiện
 #include <iostream>
 using namespace std;
  
 const int MAX = 1e6; // 1e6 = 1 * 10^6
 int cnt[MAX];
+const int ma = 1e7;
+int a[ma];
+int n;
 
-int main(){
-    int n;
-    do{
-        cout << "\nNhap n = ";
-        cin >> n;
-    }while(n < 1);
-    int a[n];
-    for(int i = 0; i < n;i++){
-        do{
-            cout << "\nNhap a[" << i << "] = ";
-            cin >> a[i];
-        }while(a[i] < 0);
-    }
-    for(int i = 0;i < MAX; i++) 
+void Find(int n) {
+    for(int i = 0; i < MAX; i++) 
         cnt[i] = 0;
-    for(int i = 0; i < n;i++){
+    for(int i = 0; i < n; i++){
         cnt[a[i]]++;
     }
-    for(int i = 0;i < MAX; i++){
-        if(cnt[i] > 0){
-            cout << "Gia tri " << i << " xuat hien " << cnt[i] << " lan!\n";
+    for(int i = 0; i < MAX; i++){
+        if(cnt[i] == 1){
+            cout << i << endl;
         }
     }
+}
+
+int main(){
+	cin >> n;
+	for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    Find(n);
+    return 0;
 }
 
 
--- UCLN_BCNN.cpp
+++ UCLN_BCNN.cpp
@@ -3,26 +3,26 @@
 #include<math.h>
 
 
-// cách thông thường
-int main() {
-    unsigned a, b, gcd, lcm;
-    printf( "Nhap cap (a, b): " );
-    scanf( "%u%u", &a, &b );
+// // cách thông thường
+// int main() {
+//     unsigned a, b, gcd, lcm;
+//     printf( "Nhap cap (a, b): " );
+//     scanf( "%u%u", &a, &b );
 
-    gcd = a;
-    while ( a % gcd || b % gcd ) {
-        gcd--;
-    }
-    printf( "USCLN (a, b): %u\n", gcd );
+//     gcd = a;
+//     while ( a % gcd || b % gcd ) {
+//         gcd--;
+//     }
+//     printf( "USCLN (a, b): %u\n", gcd );
 
-    lcm = a;
-    while ( lcm % a || lcm % b ) {
-        lcm++;
-    }
-    printf( "BSCNN (a, b): %u\n", lcm );
+//     lcm = a;
+//     while ( lcm % a || lcm % b ) {
+//         lcm++;
+//     }
+//     printf( "BSCNN (a, b): %u\n", lcm );
     
-    return 0;
-}
+//     return 0;
+// }
 
 
 // Cách khác tính UCLN bằng đệ qui
@@ -45,6 +45,8 @@ int main() {
     printf("%d", UCLN(a, b));
     printf("\n%d", BCNN(a, b));
 
+
+    return 0;
 }
 
 
